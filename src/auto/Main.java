8b3b82f957506d4775fa1eb6001715e7d0cc9749
Maxim Mitenkov <mitmax.official@gmail.com>
2024-02-28 20:44:03 +0300

    libs

--- src/auto/Main.java
+++ src/auto/Main.java
@@ -1,10 +1,22 @@
 package auto;
 
 import auto.menu.Menu;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("dfsjd"));
+
+        List<Integer> list = new ArrayList<>();
+        isEmpty(list);
+
         Menu menu = new Menu();
         menu.start(); //запустить программу
     }
