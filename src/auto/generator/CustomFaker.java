0e117607eaf421037e84ac0afc46783e65b03ce0
Maxim Mitenkov <mitmax.official@gmail.com>
2024-03-02 22:21:06 +0300

    Added CustomFaker

---
+++ src/auto/generator/CustomFaker.java
@@ -0,0 +1,9 @@
+package auto.generator;
+
+import net.datafaker.Faker;
+
+public class CustomFaker extends Faker {
+    public ModelVehicle customVehicle() {
+        return getProvider(ModelVehicle.class, ModelVehicle::new, this);
+    }
+}
